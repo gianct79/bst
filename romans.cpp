2d1876364e94a0a581a3c1b8afc408b419760842
Giancarlo Tomazelli <gianct79@gmail.com>
2013-01-19 20:22:11 +0000

    Added samples.

---
+++ romans.cpp
@@ -0,0 +1,14 @@
+/*
+* Copyright 1979-2013 Giancarlo Tomazelli. All rights reserved.
+*
+* Do not redistribute without permission.
+*/
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "romans 16:19 says..." std::endl;
+
+	return 0;
+}
