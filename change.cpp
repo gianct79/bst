e688fd75b84ee118f8ef258dc907c60720fb7fe7
Giancarlo Tomazelli <gianct79@gmail.com>
2013-01-19 20:49:47 +0000

    Added change calculator program.

---
+++ change.cpp
@@ -0,0 +1,40 @@
+/*
+* Copyright 1979-2013 Giancarlo Tomazelli. All rights reserved.
+*
+* Do not redistribute without permission.
+*/
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    int trade, money;
+
+    std::cout << "sale value: ";
+    std::cin >> trade;
+
+    std::cout << "money.....: ";
+    std::cin >> money;
+
+    int change = money - trade;
+
+    int bills = 0;
+
+    if (change) {
+
+        bills += change / 100;
+        change = change % 100;
+
+        bills += change / 50;
+        change = change % 50;
+
+        bills += change / 10;
+        change = change % 10;
+
+        bills += change / 1;
+    }
+
+    std::cout << "qty of bills: " << bills << std::endl;
+
+    return 0;
+}
--- romans.cpp
+++ romans.cpp
@@ -10,5 +10,5 @@ int main(int argc, char* argv[]) {
 
     std::cout << "romans 16:19 says..." << std::endl;
 
-	return 0;
+    return 0;
 }
