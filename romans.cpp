49b09af24b15d67a005618b9cc553fe8007edfc0
Giancarlo Tomazelli <gianct79@gmail.com>
2013-01-20 00:03:40 +0000

    Romans transformation.

--- romans.cpp
+++ romans.cpp
@@ -5,10 +5,55 @@
 */
 
 #include <iostream>
+#include <sstream>
+#include <map>
 
 int main(int argc, char* argv[]) {
 
-    std::cout << "romans 16:19 says..." << std::endl;
+    std::map<int, char*> romanDict;
 
-    return 0;
+    romanDict[1] = "I";
+    romanDict[4] = "IV";
+    romanDict[5] = "V";
+    romanDict[9] = "IX";
+    romanDict[10] = "X";
+    romanDict[40] = "XL";
+    romanDict[50] = "L";
+    romanDict[90] = "XC";
+    romanDict[100] = "C";
+    romanDict[400] = "CD";
+    romanDict[500] = "C";
+    romanDict[900] = "CM";
+    romanDict[1000] = "M";
+
+    int value;
+
+    std::cout << "enter value : ";
+    std::cin >> value;
+
+    std::map<int, size_t> romanValue;
+
+    while (value > 0) {
+
+        for (auto it = romanDict.rbegin(); it != romanDict.rend(); ++it) {
+
+            romanValue[it->first] = value / it->first;
+            value = value % it->first;
+        }
+
+        std::stringstream ss;
+
+        for (auto it = romanValue.rbegin(); it != romanValue.rend(); ++it) {
+
+            for (size_t i = 0; i < it->second; ++i)
+                ss << romanDict[it->first];
+        }
+
+        std::cout << "roman value : " << ss.str() << std::endl;
+
+        std::cout << "enter value : ";
+        std::cin >> value;
+
+        romanValue.clear();
+    }
 }
