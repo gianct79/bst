96156376210f16b4d32afc4a62b1bc0e001ecd8f
Giancarlo Tomazelli <gianct79@gmail.com>
2013-01-27 14:37:48 +0000

    Diegodie suggestion.

---
+++ change_v3.cpp
@@ -0,0 +1,45 @@
+/*
+* Copyright 1979-2013 Giancarlo Tomazelli. All rights reserved.
+*
+* Do not redistribute without permission.
+*/
+
+#include <iostream>
+#include <array>
+#include <map>
+
+int main(int argc, char* argv[]) {
+
+    std::array<int, 4> bills; bills[0] = 100; bills[1] = 50; bills[2] = 10; bills[3] = 1;
+
+    int sale, money;
+
+    std::cout << "sale value. : ";
+    std::cin >> sale;
+
+    std::cout << "money...... : ";
+    std::cin >> money;
+
+    int change = money - sale;
+
+    std::map<int, size_t> billsQty;
+
+    if (change > 0) {
+
+        for (auto it = bills.rbegin(); it != bills.rend(); ++it) {
+
+            billsQty[*it] = change / *it;
+            change = change % *it;
+        }
+
+        std::cout << "\nqty of bills: ";
+
+        for (auto it = billsQty.begin(); it != billsQty.end(); ++it)
+            std::cout << it->first << " => " << it->second << ' ';
+
+    } else {
+        std::cout << "\nno!";
+    }
+
+    return 0;
+}
