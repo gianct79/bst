aa040177db705fcf92bedb5cfe7be7be240ae78e
Giancarlo Tomazelli <gianct79@gmail.com>
2013-02-03 16:26:05 +0000

    Single linked list.

--- single_list.cpp
+++ single_list.cpp
@@ -3,147 +3,130 @@
 */
 
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
-struct node {
-    int value;
-    node *next;
-    node() {
-        next = 0;
-    }
+namespace gtlib {
+
+template <typename T>
+class single_list {
+
+	struct node {
+		T value;
+		node *next;
+
+		node(const T &value) : value(value), next(nullptr) {}
+	};
+
+	node *first_;
+	node *last_;
+
+	struct iterator : public std::iterator<forward_iterator_tag, T> {
+		iterator(node* curr) : curr_(curr) {
+		}
+		iterator& operator ++() {
+			curr_ = curr_->next; return *this;
+		}
+		const T& operator *() const {
+			return curr_->value;
+		}
+		T& operator *() {
+			return curr_->value;
+		}
+		const T* operator ->() const {
+			return &curr_->value;
+		}
+		T* operator ->() {
+			return &curr_->value;
+		}
+		bool operator ==(const iterator& other) {
+			return curr_ == other.curr_;
+		}
+		bool operator !=(const iterator& other) {
+			return curr_ != other.curr_;
+		}
+
+	private:
+		node* curr_;
+	};
+
+	public:
+	single_list() : first_(nullptr), last_(nullptr) {
+	}
+
+	~single_list() {
+		for (node *curr = first_; curr; ) {
+			node *temp = curr;
+			curr = curr->next;
+			delete temp;
+		}
+	}
+
+	void push_tail(const T &value) {
+
+		node *n = new node(value);
+
+		if (!first_)
+			first_ = n;
+		else
+			last_->next = n;
+
+		last_ = n;
+	}
+
+	const T pop_head() {
+
+		if (!first_) {
+			throw logic_error("underflow");
+		}
+
+		T v = first_->value;
+
+		node *temp = first_;
+		first_ = first_->next;
+
+		delete temp;
+
+		if (!first_) {
+			last_ = first_;
+		}
+
+		return v;
+	}
+
+	iterator begin() { return iterator(first_); }
+	iterator end() { return iterator(nullptr); }
 };
 
-struct list {
-    node *first;
+}
 
-    list() {
-        first = 0;
-    }
-
-    ~list() {
-        node *curr = first;
-        while (curr) {
-            node *temp = curr;
-            curr = curr->next;
-            delete temp;
-        }
-    }
-
-    node *add(int v) {
-        node *n = new node();
-        n->value = v;
-
-        node *last = first;
-        while (last && last->next)
-            last = last->next;
-
-        if (!last)
-            first = n;
-        else
-            last->next = n;
-
-        return n;
-    }
-
-    void remove(int v) {
-        node *prev = 0; node *curr = first;
-        while (curr) {
-            if (curr->value == v) {
-                node *temp = curr;
-                if (prev) {
-                    prev->next = curr->next;
-                    curr = prev->next;
-                } else {
-                    first = curr->next;
-                    curr = first;
-                }
-                delete temp;
-            } else {
-                prev = curr;
-                curr = curr->next;
-            }
-        }
-    }
-
-    void print() {
-        for (node *curr = first; curr; curr = curr->next) {
-            cout << curr->value << ',';
-        }
-        cout << endl;
-    }
-};
 
 int main() {
 
-    // test empty list
-    {
-        list l;
-        l.print();
-        l.remove(3);
-        l.print();
-    }
+	gtlib::single_list<int> l;
 
-    // test single element list
-    {
-        list l;
-        l.add(3);
-        l.print();
-        l.remove(3);
-        l.print();
-    }
+	for (auto it = l.begin(); it != l.end(); ++it ) {
+		cout << *it << endl;
+	}
 
-    // test element to be removed on first pos
-    {
-        list l;
-        l.add(3);
-        l.add(1);
-        l.add(2);
-        l.add(5);
-        l.print();
-        l.remove(3);
-        l.print();
-    }
+	l.push_tail(1);
+	l.push_tail(2);
+	l.push_tail(5);
 
-    // test  element to be removed on last pos
-    {
-        list l;
-        l.add(1);
-        l.add(4);
-        l.add(5);
-        l.add(3);
-        l.print();
-        l.remove(3);
-        l.print();
-    }
+	for (auto it = l.begin(); it != l.end(); ++it ) {
+		cout << *it << endl;
+	}
 
-    // test element to be removed on middle pos
-    {
-        list l;
-        l.add(8);
-        l.add(3);
-        l.add(1);
-        l.add(2);
-        l.add(3);
-        l.add(5);
-        l.print();
-        l.remove(3);
-        l.print();
-    }
+	l.pop_head();
 
-    // test element to be removed not found
-    {
-        list l;
-        l.add(8);
-        l.add(3);
-        l.add(1);
-        l.add(2);
-        l.add(3);
-        l.add(5);
-        l.print();
-        l.remove(7);
-        l.print();
-    }
+	l.push_tail(40);
+	l.push_tail(20);
+	l.push_tail(30);
 
-    return 0;
+	for (auto it = l.begin(); it != l.end(); ++it ) {
+		cout << *it << endl;
+	}
+
+	return 0;
 }
