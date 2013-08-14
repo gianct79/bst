5e4be62df3637fa45fcd24badb2d73f3c9cad63f
Giancarlo Tomazelli <gianct79@gmail.com>
2013-08-14 22:11:46 +0000

    BST

--- two_sum.cpp
+++ two_sum.cpp
@@ -10,31 +10,6 @@
 
 using namespace std;
 
-class two_sum_naive {
-
-    vector<long> _a;
-    size_t _cnt;
-
-public:
-
-    two_sum_naive(const vector<long> &a) : _a(a.begin(), a.end()), _cnt(0) {
-        sort(_a.begin(), _a.end());
-    }
-
-    size_t count(const long &sum) {
-        for (size_t i = 0; i < _a.size(); ++i) {
-            for (size_t j = 0; j < _a.size(); ++j) {
-                if (i != j && _a[i] != _a[j] && _a[i] + _a[j] == sum) {
-                    _cnt++; break;
-                }
-            }
-        }
-        return _cnt;
-    }
-
-    size_t get_count() const { return _cnt; }
-};
-
 class two_sum_bst {
 
     vector<long> _a;
@@ -86,19 +61,7 @@ int main(int argc, char* argv[]) {
     for (long l = -10000; l <= 10000; ++l) {
         two_b.count(l);
     }
-    cout << "sum  count: " << two_b.get_count() << '\n';
-
-    /*two_sum_fast two_f(list);
-    for (long l = -10000; l <= 10000; ++l) {
-        two_f.count(l);
-    }
-    cout << "sumf count: " << two_f.get_count() << '\n';
-
-    two_sum_naive two_n(list);
-    for (long l = -10000; l <= 10000; ++l) {
-        two_n.count(l);
-    }
-    cout << "sumn count: " << two_n.get_count() << '\n';*/
+    cout << "sumb count: " << two_b.get_count() << '\n';
 
     return 0;
 }
