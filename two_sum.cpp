da0eca4f8f35ba95157bef82262871095e790b7b
Giancarlo Tomazelli <gianct79@gmail.com>
2013-08-14 22:02:29 +0000

    BST

--- two_sum.cpp
+++ two_sum.cpp
@@ -6,9 +6,7 @@
 #include <fstream>
 
 #include <vector>
-#include <map>
 #include <algorithm>
-#include <chrono>
 
 using namespace std;
 
@@ -27,8 +25,7 @@ public:
         for (size_t i = 0; i < _a.size(); ++i) {
             for (size_t j = 0; j < _a.size(); ++j) {
                 if (i != j && _a[i] != _a[j] && _a[i] + _a[j] == sum) {
-                    _cnt++;
-                    break;
+                    _cnt++; break;
                 }
             }
         }
@@ -38,69 +35,31 @@ public:
     size_t get_count() const { return _cnt; }
 };
 
-class two_sum_fast {
+class two_sum_bst {
 
     vector<long> _a;
     size_t _cnt;
 
 public:
 
-    two_sum_fast(const vector<long> &a) : _a(a.begin(), a.end()), _cnt(0) {
+    two_sum_bst(const vector<long> &a) : _a(a.begin(), a.end()), _cnt(0) {
+        auto last = unique(_a.begin(), _a.end());
+        _a.erase(last + 1, _a.end());
+        sort(_a.begin(), _a.end());        
     }
 
     size_t count(const long &sum) {
-        map<long, long> d;
-        for (auto x : _a) {
-            long l = sum - x;
-            if (d.find(x) != d.end()) {
-                continue;
-            }
-            if (d.find(l) != d.end()) {
+        size_t l = 0;
+        size_t r = _a.size() - 1;
+        while (r > 0 && l < r) {
+            long s = _a[l] + _a[r];
+            if (s == sum) {
+                //cout << _a[l] << '+' << _a[r] << '=' << s << '\n'; 
                 _cnt++; break;
-            }
-            d[x] = l;
-        }
-        return _cnt;
-    }
-
-    size_t get_count() const { return _cnt; }
-};
-
-class binary_search {
-public:
-    static bool find(long key, const vector<long> &a) {
-        int lo = 0;
-        int hi = a.size() - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (key < a[mid])
-                hi = mid - 1;
-            else if (key > a[mid])
-                lo = mid + 1;
-            else
-                return true;
-        }
-        return false;
-    }
-};
-
-class two_sum_bin {
-
-    vector<long> _a;
-    size_t _cnt;
-
-public:
-
-    two_sum_bin(const vector<long> &a) : _a(a.begin(), a.end()), _cnt(0) {
-        sort(_a.begin(), _a.end());
-    }
-
-    size_t count(const long &sum) {
-        for (auto x : _a) {
-            long l = sum - x;
-            if (x != l && binary_search::find(l, _a)) {
-                _cnt++;
-                break;
+            } else if (s < sum) {
+                l++;
+            } else {
+                r--;
             }
         }
         return _cnt;
@@ -123,25 +82,17 @@ int main(int argc, char* argv[]) {
 
     cout << "list count: " << list.size() << '\n';
 
-    long cnt = 0;
-    for (size_t i = 0; i < list.size(); ++i) {
-        for (size_t j = 0; j < list.size(); ++j) {
-            cnt++;
-        }
+    two_sum_bst two_b(list);
+    for (long l = -10000; l <= 10000; ++l) {
+        two_b.count(l);
     }
-    cout << "count: " << cnt << '\n';
+    cout << "sum  count: " << two_b.get_count() << '\n';
 
     /*two_sum_fast two_f(list);
     for (long l = -10000; l <= 10000; ++l) {
         two_f.count(l);
     }
     cout << "sumf count: " << two_f.get_count() << '\n';
-
-    two_sum_bin two_b(list);
-    for (long l = -10000; l <= 10000; ++l) {
-        two_b.count(l);
-    }
-    cout << "sumb count: " << two_b.get_count() << '\n';
 
     two_sum_naive two_n(list);
     for (long l = -10000; l <= 10000; ++l) {
