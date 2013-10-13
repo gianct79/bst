9340d66493054026a963e8c7f89be5e778e5747f
Giancarlo Tomazelli <gianct79@gmail.com>
2013-10-13 14:52:19 +0000

    BST

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -13,9 +13,120 @@
 #include <stack>
 #include <climits>
 
-#include "index_min_pq"
-
 using namespace std;
+
+class index_min_pq {
+    int NMAX, N, *heap, *index, *keys;
+
+    void swap(int i, int j) {
+        int t = heap[i]; heap[i] = heap[j]; heap[j] = t;
+        index[heap[i]] = i; index[heap[j]] = j;
+    }
+
+    void bubbleUp(int k)    {
+        while (k > 1 && keys[heap[k / 2]] > keys[heap[k]])   {
+            swap(k, k / 2);
+            k = k / 2;
+        }
+    }
+
+    void bubbleDown(int k)  {
+        int j;
+        while (2 * k <= N) {
+            j = 2 * k;
+            if (j < N && keys[heap[j]] > keys[heap[j + 1]])
+                j++;
+            if (keys[heap[k]] <= keys[heap[j]])
+                break;
+            swap(k, j);
+            k = j;
+        }
+    }
+
+public:
+
+    index_min_pq(int NMAX)  {
+        this->NMAX = NMAX;
+        N = 0;
+        keys = new int[NMAX + 1];
+        heap = new int[NMAX + 1];
+        index = new int[NMAX + 1];
+        for (int i = 0; i <= NMAX; i++)
+            index[i] = -1;
+    }
+
+    ~index_min_pq() {
+        delete[] keys;
+        delete[] heap;
+        delete[] index;
+    }
+
+    bool isEmpty()  {
+        return N == 0;
+    }
+
+    bool contains(int i)    {
+        return index[i] != -1;
+    }
+
+    int size()  {
+        return N;
+    }
+
+    void insert(int i, int key) {
+        N++;
+        index[i] = N;
+        heap[N] = i;
+        keys[i] = key;
+        bubbleUp(N);
+    }
+
+    int minIndex()  {
+        return heap[1];
+    }
+
+    int minKey()    {
+        return keys[heap[1]];
+    }
+
+    int delMin() {
+        int min = heap[1];
+        swap(1, N--);
+        bubbleDown(1);
+        index[min] = -1;
+        heap[N + 1] = -1;
+        return min;
+    }
+
+    int key(int i)    {
+        return keys[i];
+    }
+
+    void change(int i, int key)  {
+        keys[i] = key;
+        bubbleUp(index[i]);
+        bubbleDown(index[i]);
+    }
+
+    void decKey(int i, int key)    {
+        keys[i] = key;
+        bubbleUp(index[i]);
+    }
+
+    void incKey(int i, int key)    {
+        keys[i] = key;
+        bubbleDown(index[i]);
+    }
+
+    void delKey(int i)   {
+        int ind = index[i];
+        swap(ind, N--);
+        bubbleUp(ind);
+        bubbleDown(ind);
+        index[i] = -1;
+    }
+};
+
 
 class edge {
 
@@ -124,14 +235,15 @@ class dijkstra_sp {
 
         for (auto e : g.adj(v)) {
             int w = e.to();
-            
+
             if (_dist_to[w] > _dist_to[v] + e.weight()) {
                 _dist_to[w] = _dist_to[v] + e.weight();
                 _edge_to[w] = e;
 
                 if (_pq.contains(w)) {
                     _pq.change(w, _dist_to[w]);
-                } else {
+                }
+                else {
                     _pq.insert(w, _dist_to[w]);
                 }
             }
@@ -139,7 +251,7 @@ class dijkstra_sp {
     }
 
 public:
-    
+
     dijkstra_sp(graph &g, const int &s) : _pq(g.m()) {
 
         for (auto e : g._adj) {
@@ -188,7 +300,7 @@ int main(int argc, char* argv[]) {
     dijkstra_sp sp(g, 1);
 
     //cout << "type vertices\n";
-    vector<int> ss = { 7,37,59,82,99,115,133,165,188,197 };
+    vector<int> ss = { 7, 37, 59, 82, 99, 115, 133, 165, 188, 197 };
     //while (cin >> s) {
     for (auto s : ss) {
         cout << sp.dist(s) << ',';
