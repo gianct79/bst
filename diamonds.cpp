8c8f8722ee65180c0ac2b133192ac85fed1990fc
Giancarlo Tomazelli <gianct79@gmail.com>
2014-07-14 21:12:47 -0300

    bst.

---
+++ diamonds.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyleft 1979-2014 GTO Inc. All rights reversed.
+ */
+
+#include <iostream>
+
+void drawLine(int spaces, int stars)
+{
+  for (; spaces > 0; spaces--)
+    std::cout << ' ';
+  for (; stars > 0; stars--)
+    std::cout << '*';
+  std::cout << '\n';
+}
+
+void drawShape(int const &lines)
+{
+  if (lines < 1)
+    return;
+
+  int spaces = lines - 1;
+  int stars = 1;
+  for (; spaces > 0; spaces--, stars += 2)
+    drawLine(spaces, stars);
+  for (; spaces < lines; spaces++, stars -= 2)
+    drawLine(spaces, stars);
+}
+
+int main()
+{
+  int lines;
+  while (std::cin >> lines)
+    drawShape(lines);
+
+  return 0;
+}
