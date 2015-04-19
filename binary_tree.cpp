c3dfc6db2025345c44694f141e7c414962b39128
Giancarlo Tomazelli <gianct79@gmail.com>
2015-04-19 09:11:10 -0300

    reformating

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -14,209 +14,209 @@ namespace gtlib {
 template <class T>
 class binary_tree {
 
-	struct Node {
-	    Node(const T &value, Node* left = nullptr, Node* right = nullptr) : value(value), left(left), right(right) {
-	    }
+    struct Node {
+        Node(const T &value, Node* left = nullptr, Node* right = nullptr) : value(value), left(left), right(right) {
+        }
 
-	    const T value;
-	    Node* left;
-	    Node* right;
-	};
+        const T value;
+        Node* left;
+        Node* right;
+    };
 
-	Node *root_;
+    Node *root_;
 
 public:
-	binary_tree() : root_(nullptr) {
-	}
+    binary_tree() : root_(nullptr) {
+    }
 
-	~binary_tree() {
-		destroy_it(root_);
-	}
+    ~binary_tree() {
+        destroy_it(root_);
+    }
 
-	Node *add(const T& v, Node *parent = nullptr) {
+    Node *add(const T& v, Node *parent = nullptr) {
 
-		if (!parent) {
-			parent = new Node(v);
-			if (!root_) {
-				root_ = parent;
-			}			
-		} else if (v < parent->value) {
-			parent->left = add(v, parent->left);
-		} else if (v > parent->value) {
-			parent->right = add(v, parent->right);
-		}
+        if (!parent) {
+            parent = new Node(v);
+            if (!root_) {
+                root_ = parent;
+            }           
+        } else if (v < parent->value) {
+            parent->left = add(v, parent->left);
+        } else if (v > parent->value) {
+            parent->right = add(v, parent->right);
+        }
 
-		return parent;
-	}
+        return parent;
+    }
 
-	Node *add_it(const T& v, Node *parent = nullptr) {
+    Node *add_it(const T& v, Node *parent = nullptr) {
 
-		Node *tmp = parent;
-		Node *prev = nullptr;
+        Node *tmp = parent;
+        Node *prev = nullptr;
 
-		while (tmp) {
-			prev = tmp;
-			if (v < tmp->value) {
-				tmp = tmp->left;
-			} else {
-				tmp = tmp->right;
-			}
-		}
+        while (tmp) {
+            prev = tmp;
+            if (v < tmp->value) {
+                tmp = tmp->left;
+            } else {
+                tmp = tmp->right;
+            }
+        }
 
-		tmp = new Node(v);
+        tmp = new Node(v);
 
-		if (!root_) {
-			root_ = tmp;
-		} else if (v < prev->value) {
-			prev->left = tmp;
-		} else if (v > prev->value) {
-			prev->right = tmp;
-		}
-		return tmp;
-	}
+        if (!root_) {
+            root_ = tmp;
+        } else if (v < prev->value) {
+            prev->left = tmp;
+        } else if (v > prev->value) {
+            prev->right = tmp;
+        }
+        return tmp;
+    }
 
-	bool search(const T& v, Node *parent = nullptr) {
-		if (parent == nullptr) {
-			return false;
-		} else if (v < parent->value) {
-			return search(v, parent->left);
-		} else if (v > parent->value) {
-			return search(v, parent->right);
-		} else {
-			return true;
-		}
-	}
+    bool search(const T& v, Node *parent = nullptr) {
+        if (parent == nullptr) {
+            return false;
+        } else if (v < parent->value) {
+            return search(v, parent->left);
+        } else if (v > parent->value) {
+            return search(v, parent->right);
+        } else {
+            return true;
+        }
+    }
 
-	bool search_it(const T& v, Node *parent = nullptr) {
+    bool search_it(const T& v, Node *parent = nullptr) {
 
-		stack<Node*> tovisit;
-		tovisit.push(parent);
+        stack<Node*> tovisit;
+        tovisit.push(parent);
 
-		while (tovisit.size() > 0) {
+        while (tovisit.size() > 0) {
 
-			const Node *curr = tovisit.top();
-			tovisit.pop();
+            const Node *curr = tovisit.top();
+            tovisit.pop();
 
-			if (curr) {
+            if (curr) {
 
-				tovisit.push(curr->left);
-				tovisit.push(curr->right);
+                tovisit.push(curr->left);
+                tovisit.push(curr->right);
 
-				if (curr->value == v) {
-					return true;
-				}
-			}
-		
-		}
+                if (curr->value == v) {
+                    return true;
+                }
+            }
+        
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	void destroy(Node *parent) {
-		if (parent) {
-			destroy(parent->left);
-			destroy(parent->right);
+    void destroy(Node *parent) {
+        if (parent) {
+            destroy(parent->left);
+            destroy(parent->right);
 
-			delete parent;
-		}
-	}
+            delete parent;
+        }
+    }
 
-	void destroy_it(Node *parent) {
+    void destroy_it(Node *parent) {
 
-		stack<Node*> tovisit;
-		tovisit.push(parent);
+        stack<Node*> tovisit;
+        tovisit.push(parent);
 
-		while (tovisit.size() > 0) {
+        while (tovisit.size() > 0) {
 
-			Node *curr = tovisit.top();
-			tovisit.pop();
+            Node *curr = tovisit.top();
+            tovisit.pop();
 
-			if (curr) {
-			
-				tovisit.push(curr->left);
-				tovisit.push(curr->right);
+            if (curr) {
+            
+                tovisit.push(curr->left);
+                tovisit.push(curr->right);
 
-				delete curr;
-			}
-		}
-	}
+                delete curr;
+            }
+        }
+    }
 
-	size_t get_height(Node *parent = nullptr) {
-		if (!parent) {
-			return 0;
-		} else {
-			return 1 + max(get_height(parent->left), get_height(parent->right));
-		}
-	}
+    size_t get_height(Node *parent = nullptr) {
+        if (!parent) {
+            return 0;
+        } else {
+            return 1 + max(get_height(parent->left), get_height(parent->right));
+        }
+    }
 
-	size_t get_height_it(Node *parent = nullptr) {
+    size_t get_height_it(Node *parent = nullptr) {
 
-		size_t height = 0;
+        size_t height = 0;
 
-		queue<Node*> visited;
-		visited.push(parent);
-		visited.push(nullptr);
+        queue<Node*> visited;
+        visited.push(parent);
+        visited.push(nullptr);
 
-		while (Node *f = visited.front()) {
-			visited.pop();
-			if (f->left) {
-				visited.push(f->left);
-		        } 
-		        if (f->right) {
-				visited.push(f->right);
-		        }
-			if (!visited.front()) {
-				visited.pop();
-				visited.push(nullptr);
-				height++;
-			}
-		}
+        while (Node *f = visited.front()) {
+            visited.pop();
+            if (f->left) {
+                visited.push(f->left);
+                } 
+                if (f->right) {
+                visited.push(f->right);
+                }
+            if (!visited.front()) {
+                visited.pop();
+                visited.push(nullptr);
+                height++;
+            }
+        }
 
-		return height;
-	}
+        return height;
+    }
 };
 
 }
 
 template <typename node_type, typename output_type>
 void print_binary_tree(output_type& out, node_type* n, size_t depth = 0, char connector = '+') {
-	if(!n) {
-		return;
-	}
+    if(!n) {
+        return;
+    }
  
-	print_binary_tree(out, n->right, depth + 1, '/');
+    print_binary_tree(out, n->right, depth + 1, '/');
  
-	for(int i = depth; i--; ) {
-		out << "    ";
-	}
-	out << ' ' << connector << "--" << n->value << endl;
+    for(int i = depth; i--; ) {
+        out << "    ";
+    }
+    out << ' ' << connector << "--" << n->value << endl;
 
-	print_binary_tree(out, n->left, depth + 1, '\\');
+    print_binary_tree(out, n->left, depth + 1, '\\');
 }
 
 int main() {
 
-	gtlib::binary_tree<int> tree;
+    gtlib::binary_tree<int> tree;
 
-	auto *root = tree.add_it(4);
+    auto *root = tree.add_it(4);
 
-	tree.add_it(2, root);
-	tree.add_it(1, root);
-	tree.add_it(3, root);
-	tree.add_it(6, root);
-	tree.add_it(5, root);
-	tree.add_it(7, root);
-	print_binary_tree(cout, root);
+    tree.add_it(2, root);
+    tree.add_it(1, root);
+    tree.add_it(3, root);
+    tree.add_it(6, root);
+    tree.add_it(5, root);
+    tree.add_it(7, root);
+    print_binary_tree(cout, root);
 
-	tree.add(10, root);
-	print_binary_tree(cout, root);
+    tree.add(10, root);
+    print_binary_tree(cout, root);
 
-	cout << tree.search_it(2, root) << endl;
-	cout << tree.search(7, root) << endl;
+    cout << tree.search_it(2, root) << endl;
+    cout << tree.search(7, root) << endl;
 
-	cout << tree.search_it(10, root) << endl;
+    cout << tree.search_it(10, root) << endl;
 
-	cout << "root height: " << tree.get_height(root) << " - " << tree.get_height_it(root) << endl;
+    cout << "root height: " << tree.get_height(root) << " - " << tree.get_height_it(root) << endl;
 
-	return 0;
+    return 0;
 }
--- change_v4.cpp
+++ change_v4.cpp
@@ -11,64 +11,64 @@ typedef int Note;
 typedef unordered_map<Note, int> Notes;
 
 class Node {
-	Notes notes_;
+    Notes notes_;
 
 public:
-	const Notes &getNotes() const {
-		return notes_;
-	}
+    const Notes &getNotes() const {
+        return notes_;
+    }
 
-	void setNote(const Note &note) {
-		notes_[note] = 0;
-	}
+    void setNote(const Note &note) {
+        notes_[note] = 0;
+    }
 
-	void addNote(const Note &note) {
-		notes_[note] += 1;
-	}
+    void addNote(const Note &note) {
+        notes_[note] += 1;
+    }
 
-	const int countNotes() const {
-		int count = 0;
-		for (auto it = notes_.begin(); it != notes_.end(); ++it) {
-			count += it->second;
-		}
-		return count;
-	}
+    const int countNotes() const {
+        int count = 0;
+        for (auto it = notes_.begin(); it != notes_.end(); ++it) {
+            count += it->second;
+        }
+        return count;
+    }
 
-	Note getReminder(const Note &change) const {
-		Note reminder = change;
-		for (auto it = notes_.begin(); it != notes_.end(); ++it) {
-			reminder -= it->first * it->second;
-		}
-		return reminder;
-	}
+    Note getReminder(const Note &change) const {
+        Note reminder = change;
+        for (auto it = notes_.begin(); it != notes_.end(); ++it) {
+            reminder -= it->first * it->second;
+        }
+        return reminder;
+    }
 
-	bool isFinal(const Note &change) const {
-		return getReminder(change) == 0;
-	}
+    bool isFinal(const Note &change) const {
+        return getReminder(change) == 0;
+    }
 
 };
 
 class Progress {
-	char curr_ = '\\';
+    char curr_ = '\\';
 
 public:
-	void step() {
-		switch (curr_) {
-		case '\\':
-			curr_ = '|';
-			break;
-		case '|':
-			curr_ = '/';
-			break;
-		case '/':
-			curr_ = '-';
-			break;
-		case '-':
-			curr_ = '\\';
-			break;
-		}
-		cout << curr_ << char(8);
-	}
+    void step() {
+        switch (curr_) {
+        case '\\':
+            curr_ = '|';
+            break;
+        case '|':
+            curr_ = '/';
+            break;
+        case '/':
+            curr_ = '-';
+            break;
+        case '-':
+            curr_ = '\\';
+            break;
+        }
+        cout << curr_ << char(8);
+    }
 
 };
 
@@ -76,64 +76,64 @@ Progress indicator;
 
 void generateSolutions(const Note &change, const Node &node, Node &solution) {
 
-	if (solution.countNotes() > 0 && node.countNotes() > solution.countNotes()) {
-		// Suggestion from diegodie: only accept solutions that could generate fewer notes
-		return;
-	}
+    if (solution.countNotes() > 0 && node.countNotes() > solution.countNotes()) {
+        // Suggestion from diegodie: only accept solutions that could generate fewer notes
+        return;
+    }
 
-	if (node.isFinal(change)) {
-		indicator.step();
-		solution = node;
-	} else if (node.getReminder(change) > 0) {
+    if (node.isFinal(change)) {
+        indicator.step();
+        solution = node;
+    } else if (node.getReminder(change) > 0) {
 
-		for (auto it = node.getNotes().begin(); it != node.getNotes().end(); ++it) {
+        for (auto it = node.getNotes().begin(); it != node.getNotes().end(); ++it) {
 
-			Note note = it->first;
+            Note note = it->first;
 
-			Node possible(node);
-			possible.addNote(note);
+            Node possible(node);
+            possible.addNote(note);
 
-			generateSolutions(change, possible, solution);
-		}
-	}
+            generateSolutions(change, possible, solution);
+        }
+    }
 }
 
 int main() {
 
-	cout << "Money : ";
-	Note money;
-	cin >> money;
+    cout << "Money : ";
+    Note money;
+    cin >> money;
 
-	cout << "Sale  : ";
-	Note sale;
-	cin >> sale;
+    cout << "Sale  : ";
+    Note sale;
+    cin >> sale;
 
-	cout << "Notes : ";
-	Node initial;
+    cout << "Notes : ";
+    Node initial;
 
-	Note n;
-	while (cin >> n) {
-		initial.setNote(n);
-	}
+    Note n;
+    while (cin >> n) {
+        initial.setNote(n);
+    }
 
-	Note change = money - sale;
+    Note change = money - sale;
 
-	cout << "Change: " << change << endl;
+    cout << "Change: " << change << endl;
 
-	if (change > 0) {
-		Node solution;
-		generateSolutions(change, initial, solution);
+    if (change > 0) {
+        Node solution;
+        generateSolutions(change, initial, solution);
 
-		for (auto it = solution.getNotes().begin(); it != solution.getNotes().end(); ++it) {
-			cout << "[" << it->first << "]: " << it->second << ", ";
-		}
-		cout << endl;
+        for (auto it = solution.getNotes().begin(); it != solution.getNotes().end(); ++it) {
+            cout << "[" << it->first << "]: " << it->second << ", ";
+        }
+        cout << endl;
 
-	} else if (change == 0) {
-		cout << "cool!" << endl;
-	} else {
-		cout << "what?" << endl;
-	}
+    } else if (change == 0) {
+        cout << "cool!" << endl;
+    } else {
+        cout << "what?" << endl;
+    }
 
-	return 0;
+    return 0;
 }
