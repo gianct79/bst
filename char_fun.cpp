4f69b233ba6a8e089dc1cee42900313d9b3c9700
Giancarlo Tomazelli <gianct79@gmail.com>
2015-10-06 12:17:44 -0300

    Update char_fun.cpp
    remove text in ()

--- char_fun.cpp
+++ char_fun.cpp
@@ -4,26 +4,60 @@
 
 #include <iostream>
 #include <stack>
+#include <vector>
 
 #include <ctype.h>
 #include <string.h>
 
 using namespace std;
 
+unsigned substr(char *str, unsigned pos, unsigned len) {
+	char *w = str + pos;
+	char *r = w + len;
+	while (r && *r)
+		*w++ = *r++;
+	if (w)
+		*w = '\0';
+	return w - str;
+}
+
+
+unsigned substr_i(char *str, unsigned pos, unsigned len) {
+	unsigned w = pos;
+	unsigned r = w + len;
+	while (str[r])
+		str[w++] = str[r++];
+	if (str[w])
+		str[w] = '\0';
+	return w;
+}
+
+unsigned remove_text_in_parenthesis(char *str, unsigned size) {
+	stack<unsigned> open;
+	for (unsigned i = 0; i < size; i++) {
+		if (str[i] == '(') {
+			open.push(i);
+		} else if (str[i] == ')' && !open.empty()) {
+			unsigned len = i - open.top();
+			open.pop();
+			i -= len;
+			size = substr(str, i, len + 1);
+		}
+	}
+	return size;
+}
+
 string reverse(const string &src) {
-
     stack <string> heap;
-
     string temp;
-    for (auto it = src.begin(); it != src.end(); ++it) {
-        temp += *it;
-        if (isspace(*it)) {
+    for (auto it : src) {
+        temp += it;
+        if (isspace(it)) {
             heap.push(temp);
             temp.erase();
         }
     }
     heap.push(temp);
-
     temp.erase();
     while (!heap.empty()) {
         temp += heap.top();
@@ -32,34 +66,29 @@ string reverse(const string &src) {
     return temp;
 }
 
-size_t countWords(const string &src) {
-
-    size_t cnt = 0;
+unsigned count_words(const string &src) {
+    unsigned cnt = 0;
     string temp;
-
-    for (auto it = src.begin(); it != src.end(); ++it) {
-        if (isspace(*it)) {
+    for (auto it : src) {
+        if (isspace(it)) {
             if (temp.length() > 0) {
                 cnt++;
                 temp.erase();
             }
         } else {
-            temp += *it;
+            temp += it;
         }
     }
     if (temp.length() > 0) {
         cnt++;
     }
-
     return cnt;
 }
 
-size_t countWords(char *src) {
-
-    size_t cnt = 0;
+unsigned count_words(char *src) {
+    unsigned cnt = 0;
     char *s = src;
     char *r = src;
-
     for (; r && *r; r++) {
         if (isspace(*r)) {
             if (s < r) {
@@ -71,21 +100,20 @@ size_t countWords(char *src) {
     if (s < r) {
         cnt++;
     }
-
     return cnt;
 }
 
-size_t countChars(const string &src, const char c) {
-    size_t cnt = 0;
-    for (auto it = src.begin(); it != src.end(); ++it) {
-        if (c == *it) {
+unsigned count_chars(const string &src, const char c) {
+    unsigned cnt = 0;
+    for (auto it : src) {
+        if (c == it) {
             cnt++;
         }
     }
     return cnt;
 }
 
-char *removeChars(char *src, const char c) {
+char *remove_chars(char *src, const char c) {
     char *s = src;
     char *w = src;
     for (char *r = src; r && *r; r++) {
@@ -94,11 +122,9 @@ char *removeChars(char *src, const char c) {
         *w = *r;
         w++;
     }
-
     if (w) {
         *w = '\0';
     }
-
     return s;
 }
 
@@ -106,39 +132,38 @@ int main() {
 
     string test("the quick brown fox jumps over the lazy dog!");
 
-    cout << reverse(test) << endl;
-    cout << reverse("") << endl;
+    cout << reverse(test) << '\n';
+    cout << reverse("") << '\n';
 
-    cout << countChars("ab*cde**fgh***", '*') << endl;
-    cout << countChars("", ' ') << endl;
+    cout << count_chars("ab*cde**fgh***", '*') << '\n';
+    cout << count_chars("", ' ') << '\n';
 
-    cout << countWords(test) << endl;
+    cout << count_words(test) << '\n';
 
-    cout << countWords("the quick brown fox jumps over the lazy dog") << endl;
-    cout << countWords("") << endl;
-    cout << countWords("  ") << endl;
-    cout << countWords(" a") << endl;
+    cout << count_words("the quick brown fox jumps over the lazy dog") << '\n';
+    cout << count_words("") << '\n';
+    cout << count_words("  ") << '\n';
+    cout << count_words(" a") << '\n';
 
-    char *stuff = new char[9];
+    vector<char> stuff(9);
 
-    strcpy(stuff, "ab*de*gh");
-    cout << removeChars(stuff, '*') << endl;
+    strcpy(&stuff[0], "ab*de*gh");
+    cout << remove_chars(&stuff[0], '*') << '\n';
 
-    strcpy(stuff, "********");
-    cout << removeChars(stuff, '*') << endl;
+    strcpy(&stuff[0], "********");
+    cout << remove_chars(&stuff[0], '*') << '\n';
 
-    strcpy(stuff, "abc efg ");
-    cout << removeChars(stuff, ' ') << endl;
+    strcpy(&stuff[0], "abc efg ");
+    cout << remove_chars(&stuff[0], ' ') << '\n';
 
-    strcpy(stuff, "");
-    cout << removeChars(stuff, ' ') << endl;
+    strcpy(&stuff[0], "");
+    cout << remove_chars(&stuff[0], ' ') << '\n';
 
-    delete[] stuff;
+    string s = {"0123(45)6789"};
+    cout << s.c_str() << '\n';
 
-    cout << removeChars(nullptr, '*') << endl;
-
-    //char *stuffa = "ab*de*gh";
-    //cout << removeChars(stuffa, '*') << endl; // pow!
+    remove_text_in_parenthesis(&s[0], test.size());
+    cout << s.c_str() << '\n';
 
     return 0;
 }
