14a9c71a1724d79e2b01b4e3ddbf5eb961aec304
Giancarlo Tomazelli <gianct79@gmail.com>
2017-04-04 20:57:08 -0300

    Create majority_element.cpp

---
+++ ideone/majority_element.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+	vector<int> v;
+	int i;
+	while (cin >> i) {
+		v.push_back(i);
+	}
+	auto n = v.size();
+    map<int, int> m; // element count
+    int ans = 0;
+    for (auto &i : v) {
+        m[i]++;
+        if (m[i] > (n/2)) {
+            // if count greater than n/2, it is major element
+            ans = i;
+        }
+    }
+    cout << ans << '\n';
+	return 0;
+}
