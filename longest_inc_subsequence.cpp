9fa5abce5caa9874a2b7df511b5d069e33906e0a
Giancarlo Tomazelli <gianct79@gmail.com>
2017-04-08 20:25:04 -0300

    Create longest_inc_subsequence.cpp

---
+++ longest_inc_subsequence.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int lis(vector<int> const &A) {
+	auto n = A.size();
+    vector<int> lis(n, 1);
+
+    int i, j, max = 0;
+
+    for (i = 1; i < n; ++i) {
+        for (j = 0; j < i; ++j) {
+            if (A[i] > A[j] && lis[i] < lis[j] + 1) {
+                lis[i] = lis[j] + 1;
+            }
+        }
+    }
+
+    for (i = 0; i < n; ++i) {
+        if (max < lis[i]) {
+            max = lis[i];
+        }
+    }
+
+    return max;
+}
+    
+int main() {
+    vector<int> A { 10, 22, 9, 33, 21, 50, 41, 60 };
+    cout << lis(A) << '\n';
+    return 0;
+}
