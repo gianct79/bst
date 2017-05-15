74457a95d20e743c7e266732963df219f491c878
Giancarlo Tomazelli <gianct79@gmail.com>
2017-05-15 15:10:00 -0300

    Update clone list.cpp

--- ideone/clone list.cpp
+++ ideone/clone list.cpp
@@ -1,45 +1,90 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 struct node {
-    int val;
+    char val;
     node *next;
+    node *rand;
 };
 
-void print(node* list) {
+ostream &operator<<(ostream &out, node* list) {
     node *curr = list;
     while (curr) {
-        cout << curr->val << ' ';
+        out << curr->val << '(' << (curr->rand ? curr->rand->val : ' ') << ") ";
         curr = curr->next;
     }
+    return out;
 }
 
-node *clone(node *list) {
+/*node *clone(node *list) {
+    unordered_map<node*, node*> m;
     node *curr = list;
     node *prev, *head = nullptr;
     while (curr) {
-        node *temp = new node { curr->val };
+        m[curr] = new node { curr->val };
         if (!head) {
-            head = temp;
+            head = m[curr];
         } else {
-            prev->next = temp;
+            prev->next = m[curr];
         }
-        prev = temp;
+        prev = m[curr];
         curr = curr->next;
     }
+
+    curr = head;
+    while (curr) {
+        curr->rand = m[list->rand];
+        curr = curr->next;
+        list = list->next;
+    }
+
     return head;
+}*/
+
+node *clone(node *list) {
+	node *curr = list;
+	// insert the new motherfuckers between the old ones
+	while (curr) {
+		curr->next = new node{ curr->val, curr->next };
+		curr = curr->next->next;
+	}
+	curr = list;
+	// adjust the fucking random pointers
+	while (curr) {
+		curr->next->rand = curr->rand ? curr->rand->next : nullptr;
+		curr = curr->next->next;
+	}
+	curr = list;
+	node *copy = curr->next;
+	node *head = copy;
+	// adjust next pointers (cleanup)
+	while (curr && copy) {
+		curr->next = curr->next ? curr->next->next : nullptr;
+		copy->next = copy->next ? copy->next->next : nullptr;
+		copy = copy->next;
+		curr = curr->next;
+	}
+	return head;
 }
 
 int main() {
+    auto e = new node {'E'};
+    auto d = new node {'D', e};
+    auto c = new node {'C', d};
+    auto b = new node {'B', c};
+    auto a = new node {'A', b};
 
-    auto n = new node {1, new node { 2, new node { 3, new node { 4 } } } };
-    print(n);
+    a->rand = d;
+    b->rand = b;
+    c->rand = a;
+    e->rand = d;
 
-    cout << '\n';
-
-    auto m = clone(n);
-    print(m);
+    cout << a << '\n';
+    auto m = clone(a);
+	delete a; delete b; delete c; delete d; delete e;
+    cout << m << '\n';
 
     return 0;
 }
